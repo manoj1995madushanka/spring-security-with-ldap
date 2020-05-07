0eabd1d634eecd74f24c3376eaa41ca6b44ef6bc
manoj1995madushanka <madushankamanoj414@gmail.com>
2020-05-07 12:27:02 +0530

    init

---
+++ src/test/java/com/example/sprintsecurityldap/SprintSecurityLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprintsecurityldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintSecurityLdapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
